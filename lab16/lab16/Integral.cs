b89d7835801be5d69a5b2ca13bbe013dbee3d9e4
SlayVic <superjamperua@gmail.com>
2022-09-07 22:10:52 +0300

    lab16

---
+++ lab16/lab16/Integral.cs
@@ -0,0 +1,46 @@
+﻿namespace lab16
+{
+  internal class Integral
+  {
+    private readonly double _a;
+    private readonly double _b;
+    private readonly int _n;
+    private readonly Func<double, double> _f;
+
+    private double? _result = null;
+    public double Result
+    {
+      get
+      {
+        _result ??= Calculate();
+        return _result.Value;
+      }
+    }
+
+    public Integral(double a, double b, int n, Func<double, double> f)
+    {
+      _a = a;
+      _b = b;
+      _n = n;
+      _f = f;
+    }
+
+    public double Calculate()
+    {
+      var h = (_b - _a) / _n;
+      var sum = 0.0;
+      for (var i = 0; i < _n; i++)
+      {
+        var x = _a + i * h;
+        sum += _f(x);
+      }
+
+      return h * sum;
+    }
+
+    public override string ToString()
+    {
+      return $"{Result}";
+    }
+  }
+}
---
+++ lab16/lab16/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace lab16
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Integral integral1 = new Integral(5, 10, 10000, x => 1 / (Math.Pow(x, 1.0 / 3.0)));
+      Console.WriteLine("integral1 = " + integral1);
+
+      Integral integral2 = new Integral(5, 10, 10000, x => 1 / (Math.Sqrt(x * x)));
+      Console.WriteLine("integral2 = " + integral2);
+
+      Integral integral3 = new Integral(5, 10, 10000, x => Math.Cos(x));
+      Console.WriteLine("integral3 = " + integral3);
+
+      var eve = new MyEventClass();
+      bool stop = false;
+      eve.EscEvent += () => stop = true;
+      while (!stop)
+      {
+      }
+    }
+  }
+
+  class MyEventClass
+  {
+    public delegate void myEventHandler();
+    public event myEventHandler? SEvent;
+    public event myEventHandler? EscEvent;
+    public void OnSEvent()
+    {
+      if (SEvent != null)
+        SEvent();
+    }
+    public void OnEscEvent()
+    {
+      if (EscEvent != null)
+        EscEvent();
+    }
+
+    public MyEventClass()
+    {
+      SEvent += () => Console.WriteLine("\bShashkov Yaroslav");
+      StartChecking();
+    }
+
+    private void StartChecking()
+    {
+      Task.Run(() =>
+      {
+        ConsoleKeyInfo cki;
+        while (true)
+        {
+          cki = Console.ReadKey();
+          switch (cki.Key)
+          {
+            case ConsoleKey.S:
+              SEvent();
+              break;
+            case ConsoleKey.Escape:
+              EscEvent();
+              break;
+          }
+
+        }
+      });
+    }
+  }
+}
